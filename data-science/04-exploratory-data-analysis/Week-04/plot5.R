9e34eac8fea1181b2e92cfa8337ead32a34c6314
Dilyan Damyanov <dilyand@gmail.com>
2018-02-11 10:20:23 +0000

    Add plot5.R and plot5.png

---
+++ data-science/04-exploratory-data-analysis/Week-04/plot5.R
@@ -0,0 +1,77 @@
+################################################################################
+# This script creates the file plot5.png, which is a required part of the      #
+# submission for the Week 4 project of the Exploratory Data Analysis course.   #
+#                                                                              #
+# By default, the script assumes you already have two files -- called          #
+# 'summarySCC_PM25.rds' and 'Source_Classification_Code.rds'                   #
+# in the current working directory.                                            #
+################################################################################
+
+
+
+################################################################################
+#
+#        READING IN THE DATA
+
+
+# Load the `dplyr` package.
+if(!require("dplyr")) {
+  install.packages("dplyr")
+  library(dplyr)
+}
+
+# Read in the data.
+nei <- tbl_df(readRDS("summarySCC_PM25.rds"))
+scc <- tbl_df(readRDS("Source_Classification_Code.rds"))
+
+
+
+################################################################################
+#
+#        DATA TRANSFORMATIONS
+
+
+# Convert SCC column to character.
+scc$SCC <- as.character(scc$SCC)
+
+# Create an index of Source Classification Codes for mobile sources.
+# This excludes recreational equipment.
+index <- scc %>% filter(grepl("Mobile Sources", SCC.Level.One)) %>% select(SCC)
+
+# Subset the data.
+nei <- nei %>% filter(SCC %in% index[[1]] & year %in% c(1999, 2008) & fips == "24510")
+
+# Calculate the total emissions from all sources by year.
+totalByYear <- nei %>% group_by(year) %>% summarize(total = sum(Emissions))
+
+# Transform the year column into factor.
+totalByYear$year <- as.factor(totalByYear$year)
+
+
+
+################################################################################
+#
+#        CREATING THE PLOT
+
+
+# Load required libraries.
+if(!require("ggplot2")) {
+  install.packages("ggplot2")
+  library(ggplot2)
+}
+
+if(!require("scales")) {
+  install.packages("scales")
+  library(scales)
+}
+
+# Plot the data and save it to a 480x480 .png file in the current working directory.
+png(file = "plot5.png", antialias = "none")
+ggplot(data = totalByYear, aes(x = year, y = total)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Total PM2.5 emissions from mobile sources in Baltimore, MD", x = "year", y = "total PM2.5 emissions, in tons") +
+  scale_y_continuous(labels = comma)
+dev.off()
+
+# Print a message to the console.
+print(paste("This script has now created a file called 'plot5.png'. Look for it in ", getwd(), "."))
